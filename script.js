88f1c6cc5b02d0cb34476440e661b6ffa10272ab
Ryan Allen <ryan.c.allen.work@gmail.com>
2021-09-28 12:14:46 -0700

    fixed html syntax error allowing questions to progress. added timer message

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var secondsLeft = 30;
 var timerElement = document.getElementById("timer-element");
 var startButton = document.getElementById("start-btn");
 var submitButton = document.getElementById("submit");
-var timerInterval
 var choiceA = document.getElementById("choice1_content");
 var choiceB = document.getElementById("choice2_content");
 var choiceC = document.getElementById("choice3_content");
@@ -12,9 +11,11 @@ var selectionContent = document.getElementById("content");
 var userAnswers = document.querySelectorAll(".user-choice");
 var quizAsk = document.getElementById("quiz-ask");
 
+// set default score and beginning quiz question to = 0
 let score = 0
 let quizNow = 0
 
+// define quiz questions within an array and use strings to assign correct answer.
 var questionArray = [
     {
         question: "What is Bangalore's TACTICAL ability?",
@@ -50,24 +51,32 @@ var questionArray = [
     },
 ];
 
+// declare function to run the quiz
 runTest()
 
+// create function to run the quiz
 function runTest() {
 
+    // declare function to remove previous answer selection from previous questions, so that answers do not carry over question after question.
     deselectAnswers()
+    // define variable to be equal to the value/title of the current question within the predefined array of questions
     var quizNowData = questionArray[quizNow]
 
+    // tell the quiz to input new text to represent the current question value/title for each following question
     quizAsk.innerText = quizNowData.question
     choiceA.innerText = quizNowData.a
     choiceB.innerText = quizNowData.b
     choiceC.innerText = quizNowData.c
     choiceD.innerText = quizNowData.d
+    console.log("started")
 }
 
+// create the function to remove answers from previous questions 
 function deselectAnswers() {
     userAnswers.forEach(userAnswer => userAnswer.checked = false)
 }
 
+// create function to return selected user answer input to then be called upon for scoring
 function inputChoice() {
     let pick 
     userAnswers.forEach(userAnswer => {
@@ -78,6 +87,7 @@ function inputChoice() {
     return pick
 }
 
+// add eventListener for submit button to determine if user selected answer was correct or incorrect --> add if statement to define parameters for correct vs incorrect --> return +/- score value,
 submitButton.addEventListener("click", () => {
     var pick = inputChoice()
     if(pick) {
@@ -86,24 +96,28 @@ submitButton.addEventListener("click", () => {
         }
         quizNow++
 
+        // add if statement to continue running quiz if remaining questions exist. If not, deploy innerHTML message stating the score + "message", and insert new 'reload' button to allow user to take quiz again.
         if(quizNow < questionArray.length) {
             runTest()
         } else {
             content.innerHTML = `
-            <h2>Your Intelligence Grants You ${score}/${questions.length} correct answers</h2>
+            <h2>Your Intelligence Grants You ${score}/${questionArray.length} correct answers</h2>
 
             <button onclick="location.reload()">Try Again If You Wish</button>
             `
         }
     }
 })
-startButton.addEventListener("click", function() {
+
+// add EventListener to submit button to initiate timer function. Deploy "message + secondsLeft" with timer countdown. If time runs out before quiz completion, deploy message to indicate user has lost.
+submitButton.addEventListener("click", function() {
     timerInterval = setInterval(() => {
         if (secondsLeft > 0) {
             secondsLeft--;
             timerElement.textContent = "System Failure in: " + secondsLeft + " seconds ";
         }
         else if (secondsLeft ===0) {
+            timerElement.textContent = "Ha Ha Ha... & You Thought You Could Beat Me? No Way. ";
             return score
         }
     }, 1000);
