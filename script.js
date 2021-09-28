822e9b2b0f3a3d38bfd11306b4f6a9ee460d0100
Ryan Allen <ryan.c.allen.work@gmail.com>
2021-09-28 10:14:05 -0700

    added js functions, start button event listener, and incorporated a functional timer

--- script.js
+++ script.js
@@ -1,18 +1,102 @@
-let score = 0;
-let quizNow = 0;
-var secondsLeft = 60;
+
+var secondsLeft = 30;
 var timerElement = document.getElementById("timer-element");
 var startButton = document.getElementById("start-btn");
-var nextButton = document.getElementById("next-btn");
+var submitButton = document.getElementById("submit");
 var timerInterval
-let quizAction = 0;
-var choice1 = document.getElementById("choice1_content");
-var choice2 = document.getElementById("choice2_content");
-var choice3 = document.getElementById("choice3_content");
-var choice4 = document.getElementById("choice4_content");
+var choiceA = document.getElementById("choice1_content");
+var choiceB = document.getElementById("choice2_content");
+var choiceC = document.getElementById("choice3_content");
+var choiceD = document.getElementById("choice4_content");
 var selectionContent = document.getElementById("content");
+var userAnswers = document.querySelectorAll(".user-choice");
 var quizAsk = document.getElementById("quiz-ask");
 
+let score = 0
+let quizNow = 0
+
+var questionArray = [
+    {
+        question: "What is Bangalore's TACTICAL ability?",
+        a: "Smoke Grenades",
+        b: "Dome Shield",
+        c: "Phase Invisible",
+        d: "Speed Boost",
+        correct: "a",
+    },
+    {
+        question: "What is Lifeline's ULTIMATE ability?",
+        a: "Fast Healing",
+        b: "Dome Shield",
+        c: "Care Package",
+        d: "Health Drone",
+        correct: "c",
+    },
+    {
+        question: "What is Bloodhound's PASSIVE ability?",
+        a: "Area Scanning",
+        b: "Changing Shape",
+        c: "Speed Boost",
+        d: "Movement Tracking",
+        correct: "d",
+    },
+    {
+        question: "What is Revenant's ULTIMATE ability?",
+        a: "Enememy Silencing",
+        b: "Death Totem",
+        c: "Boosted Climbing",
+        d: "Phase Invisible",
+        correct: "b",
+    },
+];
+
+runTest()
+
+function runTest() {
+
+    deselectAnswers()
+    var quizNowData = questionArray[quizNow]
+
+    quizAsk.innerText = quizNowData.question
+    choiceA.innerText = quizNowData.a
+    choiceB.innerText = quizNowData.b
+    choiceC.innerText = quizNowData.c
+    choiceD.innerText = quizNowData.d
+}
+
+function deselectAnswers() {
+    userAnswers.forEach(userAnswer => userAnswer.checked = false)
+}
+
+function inputChoice() {
+    let pick 
+    userAnswers.forEach(userAnswer => {
+        if(userAnswer.checked) {
+            pick = userAnswer.id
+        }
+    })
+    return pick
+}
+
+submitButton.addEventListener("click", () => {
+    var pick = inputChoice()
+    if(pick) {
+        if(pick === questionArray[quizNow].correct) {
+            score++
+        }
+        quizNow++
+
+        if(quizNow < questionArray.length) {
+            runTest()
+        } else {
+            content.innerHTML = `
+            <h2>Your Intelligence Grants You ${score}/${questions.length} correct answers</h2>
+
+            <button onclick="location.reload()">Try Again If You Wish</button>
+            `
+        }
+    }
+})
 startButton.addEventListener("click", function() {
     timerInterval = setInterval(() => {
         if (secondsLeft > 0) {
@@ -23,50 +107,7 @@ startButton.addEventListener("click", function() {
             return score
         }
     }, 1000);
-})
-
-var questions = [
-    {
-        question: "What is Bangalore's TACTICAL ability?",
-        1: "Smoke Grenades",
-        2: "Dome Shield",
-        3: "Phase Invisible",
-        4: "Speed Boost",
-        correct: "1",
-    },
-    {
-        question: "What is Lifeline's ULTIMATE ability?",
-        1: "Fast Healing",
-        2: "Dome Shield",
-        3: "Care Package",
-        4: "Health Drone",
-        correct: "3",
-    },
-    {
-        question: "What is Bloodhound's PASSIVE ability?",
-        1: "Area Scanning",
-        2: "Changing Shape",
-        3: "Speed Boost",
-        4: "Movement Tracking",
-        correct: "4",
-    },
-    {
-        question: "What is Revenant's ULTIMATE ability?",
-        1: "Enememy Silencing",
-        2: "Death Totem",
-        3: "Boosted Climbing",
-        4: "Phase Invisible",
-        correct: "2",
-    },
-];
-
-// runTest()
-
-// function runTest()
-// var showQuizQuestions = questions[quizNow]
-
-// quizAsk.innerText = showQuizQuestions.question
-// choice1.innerText
+});
 
 
 
