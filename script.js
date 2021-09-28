f3bc6b2307220455fd577e09331bfcd3a46ef99a
Ryan Allen <ryan.c.allen.work@gmail.com>
2021-09-28 03:10:22 -0700

    added/defined majority of variables in js

---
+++ script.js
@@ -0,0 +1,11 @@
+let score = 0;
+var secondsLeft = 60;
+var startButton = document.getElementById("start-btn");
+var nextButton = document.getElementById("next-btn");
+var timerInterval = 0;
+let quizAction = 0
+var choice2 = document.getElementById("choice2_content");
+var choice2 = document.getElementById("choice2_content");
+var choice3 = document.getElementById("choice3_content");
+var choice4 = document.getElementById("choice4_content");
+var 
